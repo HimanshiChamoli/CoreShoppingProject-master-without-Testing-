2ea9b3ed9c235b44a07b946ccbf7b7f58fd1b201
Himanshi <Himanshi.3@NIIT-Tech.com>
2019-04-16 20:08:52 +0530

    WithoutTesting

---
+++ EcommerceUserPanel/Controllers/BrandController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceUserPanel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceUserPanel.Controllers
+{
+    public class BrandController : Controller
+    {
+        ShoppingDemoooo2Context context = new ShoppingDemoooo2Context();
+        public IActionResult Index()
+        {
+            var brand = context.Brands.ToList();
+            return View(brand);
+        }
+        public IActionResult ProductDisplay(int id)
+        {
+            var p = context.Products.Where(x => x.BrandId == id).ToList();
+            return View(p);
+        }
+    }
+}
---
+++ EcommerceUserPanel/Controllers/ProductCategoryController.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceUserPanel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceUserPanel.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+         
+        ShoppingDemoooo2Context context = new ShoppingDemoooo2Context();
+        public IActionResult Index()
+        {
+
+            var productcat = context.Categories.ToList();
+            return View(productcat);
+        }
+        public IActionResult Display(int id)
+        {
+            var product = context.Products.Where(x => x.ProductCategoryId == id).ToList();
+            return View(product);
+        }
+    }
+}
